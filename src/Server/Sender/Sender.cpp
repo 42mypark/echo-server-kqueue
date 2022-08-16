60694d3e2225ec555e358f8dd54696282ad85f3d
Myeonghyeon Park <mypark@c1r4s4.42seoul.kr>
2022-08-16 19:00:28 +0900

    fix

--- src/Server/Sender/Sender.cpp
+++ src/Server/Sender/Sender.cpp
@@ -5,7 +5,8 @@ void Sender::sendClient(std::vector<EventInfo*>& event_list) {
 
   for (; it != event_list.end(); ++it) {
     EventInfo&  event_info = **it;
-    std::string to_send    = event_info.storage().str();
+    std::string to_send;
+    event_info.storage() >> to_send;
     std::cout << "Server: send to client: '" << to_send << "' length: " << to_send.length()
               << std::endl;
     send(event_info.toFd(), to_send.c_str(), to_send.length(), 0);
